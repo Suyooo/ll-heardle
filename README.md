1e68d915a9f82601eb6adfc4869f3f0ea905177e
Suyooo <me@suyo.be>
2022-10-10 18:24:30 +0200

    Some progress

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
 # Love Live Heardle
 
-...or, more generally, a base for a Heardle game using a list of YouTube videos. There's some comments in some places.
-
-The `vendor` directory contains:
-* [jQuery](https://github.com/jquery/jquery)
-* [Fuse.JS](https://github.com/krisk/Fuse) (fuzzy search)
-* [fuzzycomplete](https://github.com/aliask/fuzzycomplete) (jQuery plugin to use Fuse.JS for autocompleting an input)
-* [Skeleton](http://getskeleton.com) (CSS boilerplate)
+Runs the original Heardle site, but using HTML5 Audio elements instead of Soundcloud.
--- js/main.js
+++ js/main.js
@@ -10,6 +10,30 @@ const RESETTIME = 1665154800000;
 // Use unix timestamp to figure out what day it is in JST
 const DAY = Math.floor((new Date().getTime() - RESETTIME) / (1000 * 60 * 60 * 24));
 
+/*****
+ * Grab element references
+ ****/
+
+const $playerbar = $("#playerbar");
+const $guesslistChildren = $("#guesslist").children().toArray().map(e => $(e));
+const $loading = $("#loading");
+const $playprompt = $("#playprompt");
+const $audio = $("#audio_player");
+const audio = $audio[0];
+
+const $guessbar = $("#guessbar");
+const $field = $("#field");
+const $fieldclear = $("#fieldclear");
+const $skipbutton = $("#skipbutton");
+const $submitbutton = $("#submitbutton");
+const $control = $("#control");
+
+const $timecurrent = $("#timecurrent");
+const $playbarcurrent = $("#playbarcurrent");
+const $timelimit = $("#timelimit");
+const $playbarlimit = $("#playbarlimit");
+const $playbarmarkersChildren = $("#playbarmarkers").children().toArray().map(e => $(e));
+
 
 /*****
  * Pseudo Random Number Generator from https://stackoverflow.com/a/19301306
@@ -21,14 +45,12 @@ let m_w = 123456789;
 let m_z = 987654321;
 const mask = 0xffffffff;
 
-// Takes any integer
 function prngSeed(i) {
     m_w = (123456789 + i) & mask;
     m_z = (987654321 - i) & mask;
 }
 
-// Returns number between 0 (inclusive) and 1.0 (exclusive),
-// just like Math.random().
+// Returns number between 0 (inclusive) and 1.0 (exclusive), just like Math.random().
 function prngRandom() {
     m_z = (36969 * (m_z & 65535) + (m_z >> 16)) & mask;
     m_w = (18000 * (m_w & 65535) + (m_w >> 16)) & mask;
@@ -81,18 +103,13 @@ if (lastDay === null || DAY > parseInt(lastDay)) {
     saveTodayState();
     stats.viewed += 1;
     saveStats();
+    prepareNextGuess();
 } else {
-    const guessList = $("#guesses");
-    for (const guess of state.guesses) {
-        const guessEl = document.createElement('div');
-        guessEl.innerHTML = guess;
-        guessList.append(guessEl);
-    }
-    $("#guesscount").text("Guess " + (state.failed === 6 ? 6 : state.failed + 1) + " of 6");
-    // TODO buttonlabel();
-
     if (state.finished) {
         // TODO reveal(state.cleared, state.guesses.at(-1));
+    } else {
+        state.guesses.forEach((guess, guessNo) => showWrongGuess(guessNo, guess));
+        prepareNextGuess();
     }
 }
 
@@ -132,19 +149,23 @@ const THE_CORRECT_ONE = CURRENT_HEARDLE.titleEn + " - " + CURRENT_HEARDLE.artist
  * Audio Player
  ****/
 
-const audio = $("#audio_player")[0];
-audio.addEventListener("timeupdate", (event) => {
+function playerTimeUpdate(event) {
+    $timecurrent.text((audio.currentTime < 10 ? "0:0" : "0:") + Math.floor(audio.currentTime));
+    $playbarcurrent.width((audio.currentTime / (state.finished ? audio.duration : LENGTHS[state.failed]) * 100) + "%");
     if (!state.finished && audio.currentTime >= LENGTHS[state.failed]) {
         playerReset();
+    } else {
+        requestAnimationFrame(playerTimeUpdate);
     }
-});
+}
 
-$(audio)
+$audio
     .attr("src", "https://static.wikia.nocookie.net/love-live/images/9/9f/Oh%2CLove%26Peace!.ogg")
     .one("canplay", () => {
-        showPlayerBar();
+        $loading.hide();
+        $playerbar.show();
     });
-$("#control").on("click", controlClicked);
+$control.on("click", controlClicked);
 
 function controlClicked() {
     if (audio.duration > 0 && !audio.paused) {
@@ -156,6 +177,8 @@ function controlClicked() {
 
 function playerPlay() {
     audio.play();
+    $playprompt.hide();
+    requestAnimationFrame(playerTimeUpdate);
 }
 
 function playerReset() {
@@ -175,25 +198,29 @@ setInterval(() => {
  * Guessing
  ****/
 
-$("#skipbutton").on("click", () => {
-    resolveGuess(false, null);
+$skipbutton.on("click", () => {
+    resolveGuess(state.failed, false, null);
 });
 
-$("#submitbutton").on("click", () => {
-    const guess = songfield.value;
+$submitbutton.on("click", () => {
+    const guess = $field.val();
     // Block input that is not an autocompleted option
     const option = [0]; // TODO $("select[name='song'] option").toArray().filter(e => e.innerText === guess);
     if (option.length > 0) {
         // addToStatistics() is called in the guess submission method instead of reveal()
         // so it is guaranteed a round only gets added to statistics exactly once
         if (guess === THE_CORRECT_ONE) {
-            resolveGuess(true, guess);
+            resolveGuess(state.failed, true, guess);
         } else {
-            resolveGuess(false, guess);
+            resolveGuess(state.failed, false, guess);
         }
     } else {
         // TODO $("#note").text("Please pick one of the options (or leave the field empty to skip)");
     }
+});
+
+$fieldclear.on("click", () => {
+    $field.val("");
 });
 
 
@@ -201,7 +228,9 @@ $("#submitbutton").on("click", () => {
  * UI
  ****/
 
-function resolveGuess(wasCorrect, guess) {
+function resolveGuess(guessNo, wasCorrect, guess) {
+    if (state.finished) return;
+    state.guesses.push(guess);
     if (wasCorrect) {
         state.cleared = true;
         reveal(true, guess);
@@ -212,42 +241,72 @@ function resolveGuess(wasCorrect, guess) {
             reveal(false, guess);
             addToStatistics();
         } else {
-            const guesslistRow = $($("#guesslist").children()[state.failed - 1]);
-            const symbol = guess === null ? symbolSkip() : symbolIncorrect();
-            const text = guess === null ? styleGuessSkipped().text("SKIPPED") : styleGuessSong().text(guess);
-            guesslistRow
-                .append($(`<div class="mr-2"></div>`).append(symbol))
-                .append($(`<div class="flex flex-1 justify-between items-center"></div>`).append(text))
-                .removeClass("border-custom-line");
+            showWrongGuess(guessNo, guess);
             prepareNextGuess();
         }
     }
     saveTodayState();
 }
 
+function showWrongGuess(guessNo, guess) {
+    const guesslistRow = $guesslistChildren[guessNo];
+    const symbol = guess === null ? symbolSkip() : symbolIncorrect();
+    const text = guess === null ? styleGuessSkipped().text("SKIPPED") : styleGuessSong().text(guess);
+    guesslistRow
+        .append($(`<div class="mr-2"></div>`).append(symbol))
+        .append($(`<div class="flex flex-1 justify-between items-center"></div>`).append(text))
+        .removeClass("border-custom-line");
+}
+
 function prepareNextGuess() {
-    const guesslistNextRow = $($("#guesslist").children()[state.failed]);
-    guesslistNextRow.addClass("border-custom-line");
+    playerReset();
     updateSkipLabel();
+
+    // Show "Click Play" prompt if no guesses have been made yet
+    $playprompt.toggle(state.failed === 0);
+
+    // Mark current guess list row
+    $guesslistChildren[state.failed].addClass("border-custom-line");
+
+    // Show timelimit in / next to play bar
+    $playbarlimit.width((LENGTHS[state.failed] / LENGTHS.at(-1) * 100) + "%");
+    $playbarmarkersChildren.forEach(($element, index) => {
+        if (index <= state.failed) {
+            $element.removeClass("bg-custom-line bg-custom-mg").addClass("bg-custom-bg");
+        } else if (index === state.failed + 1) {
+            $element.removeClass("bg-custom-mg").addClass("bg-custom-line");
+        }
+    });
+    $timelimit.text((LENGTHS[state.failed] < 10 ? "0:0" : "0:") + LENGTHS[state.failed]);
 }
 
 function updateSkipLabel() {
     if (state.failed === 5) {
-        $("#skiplength").text("(GIVE UP)");
+        $skipbutton.text("GIVE UP");
     } else {
         const diff = LENGTHS[state.failed + 1] - LENGTHS[state.failed];
-        $("#skiplength").text("(+" + diff + "s)");
+        $skipbutton.text("SKIP (+" + diff + "s)");
     }
 }
 
 function reveal(success, lastGuess) {
+    // Disallow guessing
     state.finished = true;
-    $("#player").show();
+    $guessbar.hide();
+
+    // Show full song and play
+    $playbarlimit.width("100%");
+    $playbarmarkersChildren.forEach(($element) => $element.remove());
+    const fullSongSeconds = Math.floor(audio.duration % 60);
+    $timelimit.text(Math.floor(audio.duration / 60) + ":" + (fullSongSeconds < 10 ? "0" : "") + fullSongSeconds);
+    playerReset();
+    playerPlay();
+
     $("#guessing").html("<span style='font-size:200%'>" + (success ? "you did it!" : "oops") + "</span>");
     $("#copybuttons").show();
 
     const copything = $("#copything")[0];
-    copything.innerHTML = SHARELABEL + "<br>🔈";
+    copything.innerHTML = "🔈";
 
     for (let i = 0; i < (success ? state.failed : 5); i++) {
         if ($("#guesses")[0].children[i]?.innerHTML === "⬜ (skip)")
@@ -271,8 +330,6 @@ function reveal(success, lastGuess) {
         copything.innerHTML += " X/6";
     }
     copything.innerHTML += "<br>" + SHAREPOST;
-
-    if (player) player.loadVideoById({'videoId': VID});
 }
 
 function share() {
@@ -324,11 +381,11 @@ Object.defineProperty(songfield, "value", {
  ****/
 
 function symbolSkip() {
-    return $(`<svg xmlns="http://www.w3.org/2000/svg" class="text-custom-mg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect></svg>`)
+    return $(`<svg xmlns="http://www.w3.org/2000/svg" class="text-custom-mg w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect></svg>`)
 }
 
 function symbolIncorrect() {
-    return $(`<svg class="text-custom-negative" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`)
+    return $(`<svg xmlns="http://www.w3.org/2000/svg" class="text-custom-negative w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`)
 }
 
 function styleGuessSkipped() {

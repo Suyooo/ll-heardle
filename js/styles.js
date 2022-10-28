e2c9524caf905268acefb05e4c89caea64b23f73
Suyooo <me@suyo.be>
2022-10-28 17:16:54 +0200

    Move assets to main glitch page

--- js/styles.js
+++ js/styles.js
@@ -3,11 +3,11 @@
  ****/
 
 function symbolSkip() {
-    return $(`<svg xmlns="http://www.w3.org/2000/svg" class="text-custom-mg w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect></svg>`)
+    return $(`<svg xmlns="http://www.w3.org/2000/svg" role="img" class="text-custom-mg w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Skipped"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect></svg>`)
 }
 
 function symbolIncorrect() {
-    return $(`<svg xmlns="http://www.w3.org/2000/svg" class="text-custom-negative w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`)
+    return $(`<svg xmlns="http://www.w3.org/2000/svg" role="img" class="text-custom-negative w-5 h-5" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-label="Wrong"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>`)
 }
 
 function styleGuessRowSymbol() {
